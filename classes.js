b09f69e9ffeae23b212b43e57f9c2da54a1714d3
Michael Bischoff <wmichealbischoff@gmail.com>
2019-01-30 22:36:28 -0800

    update

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+  }
 
 
 ////////// PROBLEM 2 //////////
@@ -50,6 +60,24 @@
 */
 
 //Code Here
+class Manager {
+  constructor(first_name, last_name, email, age , reports){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = (reports)? [reports]: []
+  }
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+  hire(employee){
+this.reports.push(employee)
+  }
+  fire(index){
+    this.reports.splice([index], 1)
+  }
+}
 
 
 
